2f489afaf71ec6ceacd92260c4af12be6ea6fbf4
Raghav Aggarwal <raghavaggsss@gmail.com>
2019-06-17 15:22:58 -0700

    SEI

--- leaflet_map.js
+++ leaflet_map.js
@@ -17,46 +17,92 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     }).addTo(map);
 
 
-function ajax (keyword) { //AJAX request
-	$.ajax({
-		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
-		dataType: "jsonp",
-		success: function(response) {
-			// console.log(response.query);
-			if (response.query.searchinfo.totalhits === 0) {
-				alert("No results");
-			}
 
-			else {
-				showResults(response);
-			}
-		},
-		error: function () {
-			alert("Error retrieving search results, please refresh the page");
-		}
-	});
-}
-
-function showResults(callback) {
+function openWiki(callback, speciesName) {
     var title = callback.query.search[0].title;
+
     var url = title.replace(/ /g, "_");
-    window.open("https://en.wikipedia.org/wiki/" + url, "_blank");
+
+    var speciesId = speciesName.replace(/\s+/g, '_');
+
+    var wikiButton = document.getElementById(speciesId + "_wiki");
+    wikiButton.onclick = function () {window.open("https://en.wikipedia.org/wiki/" + url, "_blank");};
+    wikiButton.value = "Wikipedia";
+}
+
+
+function loadImg(speciesName) { //AJAX request
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + speciesName + "&prop=info&inprop=url&utf8=&format=json",
+        dataType: "jsonp",
+        success: function (response) {
+            // console.log(response.query);
+            if (response.query.searchinfo.totalhits === 0) {
+                alert("No results");
+            } else {
+                wikiImg(response.query.search[0].title, speciesName);
+                openWiki(response, speciesName);
+            }
+        },
+        error: function () {
+            alert("Error retrieving search results, please refresh the page");
+        }
+    });
 }
 
 
 
-// openNewWindow = function(species)
-// {
-//
-//  window.open("http://www.google.com/", "_blank");
-// };
+function wikiImg(pageTitle, speciesName) { //AJAX request
+    $.ajax({
+        url: "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&sites=enwiki&props=claims&titles=" + pageTitle,
+        dataType: "jsonp",
+        success: function (response) {
+            // console.log(response.query);
+            // if (response.query.searchinfo.totalhits === 0) {
+            // 	showError(keyword);
+            // }
+
+            // else {
+            imgResults(response, speciesName);
+            // }
+        },
+        error: function () {
+            alert("Error retrieving search results, please refresh the page");
+        }
+    });
+}
+
+function imgResults(callback, speciesName) {
+
+    var img_name = callback.entities;
+    var key_entity = Object.keys(img_name)[0];
+    img_name = callback.entities[key_entity].claims.P18[0].mainsnak.datavalue.value;
+
+    img_name = img_name.replace(/\s+/g, '_');
+    // console.log(img_name);
+    var img_name_hash = md5(img_name);
+    var img_url = 'https://upload.wikimedia.org/wikipedia/commons/' + img_name_hash[0] +
+        '/' + img_name_hash.substr(0, 2) + '/' + img_name;
+    // console.log(img_url);
+    var speciesId = speciesName.replace(/\s+/g, '_');
+
+    var img = new Image(),
+        url = img_url,
+        container = document.getElementById( speciesId + "_thumb");
+
+    img.onload = function () { container.appendChild(img); };
+    img.src = url;
+    // return img_url;
+
+
+}
 
 // load GeoJSON from an external file
-// var points_data;
+var points_data;
 $.getJSON("gbif_tot.geojson", function (data) {
     var dotIcon = L.icon({
-      iconUrl: 'blue_dot.png',
-      iconSize: [10,10]
+        iconUrl: 'blue_dot.png',
+        iconSize: [10, 10]
     });
     // add GeoJSON layer to the map once the file is loaded
     var points_data = L.geoJson(data, {
@@ -65,45 +111,62 @@ $.getJSON("gbif_tot.geojson", function (data) {
             // var marker = L.marker(latlng, {title: ""});
             var redList = feature.properties.redList;
             var markerPopUp = '<div class="popUpfeature">' + feature.properties.species + '<br/> <b>'
-                + feature.properties.common + '</b>';
+                + feature.properties.common.split(',')[0] + '</b>';
             if (redList) {
-                markerPopUp += '<div class="redListFont">' + '<br/>' + feature.properties.redList + '</div>';
+                markerPopUp += '<b class="redListFont">' + '<br/>' + feature.properties.redList + '</b>';
             }
 
             markerPopUp += '<br/>';
 
-            markerPopUp += '<input type="button" value="Wiki" onClick="ajax(\'' + feature.properties.species + '\')" />';
+            var id_species  = feature.properties.species;
+            if (id_species) {
+                id_species = id_species.replace(/\s+/g, '_');
+            }
+
+            // markerPopUp += '<input type="button" value="wikiImg" '+  ' onClick="loadImg(\'' + feature.properties.species + '\')" />';
+
+            // markerPopUp += '<input type="button" value="Wiki" '+ ' onClick="goToWiki(\'' + feature.properties.species + '\')" />';
+
+            markerPopUp += '<input type="button" class="wikiButton" value="Loading" id=' + (id_species + "_wiki") + '>';
+
+            markerPopUp += '<div class="thumbnail"  id=' + (id_species + "_thumb") + '> </div>';
 
             markerPopUp += '</div>';
 
             marker.bindPopup(markerPopUp);
+
+            marker.on('click', function() {
+                loadImg(feature.properties.species);
+            });
+
             return marker;
         }
     });
     var clusters = L.markerClusterGroup(
-        {iconCreateFunction: function(cluster) {
-            var i = 0;
-            var childrenMarkers = cluster.getAllChildMarkers();
-            var endangered = 0;
-            for (i = 0; i < childrenMarkers.length; i++) {
-                if (childrenMarkers[i].feature.properties.redList) {
-                    endangered = 1;
-                    break;
+        {
+            iconCreateFunction: function (cluster) {
+                var i = 0;
+                var childrenMarkers = cluster.getAllChildMarkers();
+                var endangered = 0;
+                for (i = 0; i < childrenMarkers.length; i++) {
+                    if (childrenMarkers[i].feature.properties.redList) {
+                        endangered = 1;
+                        break;
+                    }
                 }
-            }
-            var num_children = cluster.getChildCount();
-            if (num_children == 1) {
-                num_children = "";
-            }
-            if (endangered) {
-                return L.divIcon({ html: num_children, className: 'endangered', iconSize: 15});
-            }
-            else {
-                return L.divIcon({ html: num_children, className: 'not_endangered', iconSize: 15 });
-            }
+                var num_children = cluster.getChildCount();
+                if (num_children == 1) {
+                    num_children = "";
+                }
+                if (endangered) {
+                    return L.divIcon({html: num_children, className: 'endangered', iconSize: 15});
+                } else {
+                    return L.divIcon({html: num_children, className: 'not_endangered', iconSize: 15});
+                }
 
 
-	}, singleMarkerMode: 1}
+            }, singleMarkerMode: 1
+        }
     );
     clusters.addLayer(points_data);
     map.addLayer(clusters);
@@ -121,16 +184,58 @@ $.getJSON("gbif_tot.geojson", function (data) {
 //   }  ).addTo(map);
 // });
 
+var highlight = {
+    'color': '#333333',
+    'weight': 1,
+    'opacity': 1,
+    'fillColor': '#14bd00'
+};
+
+var remove_highlight = {
+    'color': "#515055",
+    'weight': 1,
+    'fillColor': "#8d8c91",
+    'fillOpacity': .5};
+
 
 // add shapes
-$.getJSON("UBC_poly.geojson", function (hoodData) {
-    L.geoJson(hoodData, {
+$.getJSON("SEI.geojson", function (data) {
+    L.geoJson(data, {
         style: function (feature) {
-            return {color: "#e10c1c", weight: 1, fillColor: "#66ff33", fillOpacity: .3};
+            return remove_highlight;
         },
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("<strong>" + feature.properties.PROTECTED_ + "</strong><br/>")
+            var popUpInfo =  feature.properties.Comp1Lgnd_;
+            if (feature.properties.Comp2Lgnd_) {
+                popUpInfo += "<br/>" + feature.properties.Comp2Lgnd_;
+                if (feature.properties.Comp3Lgnd_) {
+                    popUpInfo += "<br/>" + feature.properties.Comp3Lgnd_;
+                }
+            }
+            popUpInfo += "</br> Quality: " + feature.properties.QualityNo_ + "/5.0";
+            if (feature.properties.Location) {
+                popUpInfo += "</br> Location: " + feature.properties.Location;
+            }
+            // var popUp = layer.bindPopup(popUpInfo);
+            // popUp.on('popupclose', function() {
+            //     // layer.setStyle(remove_highlight);
+            // });
+            layer.on('click', function() {
+                layer.bringToFront();
+                if (layer.selected) {
+                    layer.setStyle(remove_highlight);
+                    layer.selected = 0;
+                    layer.unbindPopup();
+                }
+                else {
+                    layer.setStyle(highlight);
+                    layer.selected = 1;
+                    layer.bindPopup(popUpInfo).openPopup();
+                }
+            })
         }
     })
         .addTo(map);
 });
+
+
