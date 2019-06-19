30b7c4eb78f7a6e9f671ed1279cb7dfc3a5b8872
Raghav Aggarwal <raghavaggsss@gmail.com>
2019-06-19 15:03:11 -0700

    sei and split gbif by year

--- leaflet_map.js
+++ leaflet_map.js
@@ -1,22 +1,108 @@
+L.TopoJSON = L.GeoJSON.extend({
+    addData: function (data) {
+        var geojson, key;
+        if (data.type === "Topology") {
+            for (key in data.objects) {
+                if (data.objects.hasOwnProperty(key)) {
+                    geojson = topojson.feature(data, data.objects[key]);
+                    L.GeoJSON.prototype.addData.call(this, geojson);
+                }
+            }
+
+            return this;
+        }
+
+        L.GeoJSON.prototype.addData.call(this, data);
+
+        return this;
+    }
+});
+
+L.topoJson = function (data, options) {
+    return new L.TopoJSON(data, options);
+};
+
+function countKeys(obj) {
+    return Object.keys(obj).length;
+}
+
+
+var highlight = {
+    'color': '#ffffff',
+    'weight': 2,
+    'opacity': 1,
+    'fillColor': '#ffffff',
+    'fillOpacity': 0.5
+};
+
+var remove_highlight = {
+    'color': "#d2d2d2",
+    'weight': 1,
+    'fillColor': "#8d8c91",
+    'fillOpacity': 1
+};
+
+
+var typeEco = {
+    "ME": "#F012BE",
+    "SE": "#85144b",
+    "XX": "rgb(39,39,48)",
+    "YS": "#c9c731"
+};
+
+var typeSE = {
+    "OF": "#2ECC40",
+    "MF": "#3D9970",
+    "WD": "#653f2e",
+    "RI": "#00236e",
+    "IT": "#eeaf8c",
+    "WN": "#0074D9",
+    "HB": "#98Fb98",
+    "SV": "#857e4e",
+    "ES": "#7FDBFF",
+    "FW": "#39CCCC",
+    "AP": "#AAAAAA",
+
+};
+
 // initialize the map
-var map = L.map('map').setView([49.263710, -123.259378], 13);
 
-// load a tile layer
-// L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-//   {
-//     attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-//     maxZoom: 17,
-//     minZoom: 9
-//   }).addTo(map);
 
-// openstreet maps
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+var cartodbAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+
+// 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+grayScaleBaseMap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
     {
+        edgeBufferTiles: 1,
+        attribution: cartodbAttribution,
         maxZoom: 17,
         minZoom: 2
-    }).addTo(map);
+    });
 
 
+streetsBaseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        edgeBufferTiles: 1,
+        attribution: cartodbAttribution,
+        maxZoom: 17,
+        minZoom: 2
+    });
+
+var baseLayers = {
+    "Grayscale": grayScaleBaseMap,
+    "Streets": streetsBaseMap
+};
+
+var overlays = {};
+var num_overlays = 1;
+
+var map = L.map('map', {
+    center: [49.263710, -123.259378],
+    zoom: 10,
+    layers: [streetsBaseMap, grayScaleBaseMap]
+});
+
+grayScaleBaseMap.addTo(map);
 
 function openWiki(callback, speciesName) {
     var title = callback.query.search[0].title;
@@ -26,7 +112,9 @@ function openWiki(callback, speciesName) {
     var speciesId = speciesName.replace(/\s+/g, '_');
 
     var wikiButton = document.getElementById(speciesId + "_wiki");
-    wikiButton.onclick = function () {window.open("https://en.wikipedia.org/wiki/" + url, "_blank");};
+    wikiButton.onclick = function () {
+        window.open("https://en.wikipedia.org/wiki/" + url, "_blank");
+    };
     wikiButton.value = "Wikipedia";
 }
 
@@ -49,7 +137,6 @@ function loadImg(speciesName) { //AJAX request
         }
     });
 }
-
 
 
 function wikiImg(pageTitle, speciesName) { //AJAX request
@@ -88,27 +175,91 @@ function imgResults(callback, speciesName) {
 
     var img = new Image(),
         url = img_url,
-        container = document.getElementById( speciesId + "_thumb");
+        container = document.getElementById(speciesId + "_thumb");
 
-    img.onload = function () { container.appendChild(img); };
+    img.onload = function () {
+        container.appendChild(img);
+    };
     img.src = url;
     // return img_url;
-
-
 }
 
-// load GeoJSON from an external file
-var points_data;
-$.getJSON("gbif_tot.geojson", function (data) {
-    var dotIcon = L.icon({
-        iconUrl: 'blue_dot.png',
-        iconSize: [10, 10]
-    });
-    // add GeoJSON layer to the map once the file is loaded
-    var points_data = L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            var marker = L.marker(latlng, {icon: dotIcon});
-            // var marker = L.marker(latlng, {title: ""});
+var dotIcon = L.icon({
+    iconUrl: 'blue_dot.png',
+    iconSize: [10, 10]
+});
+
+
+var shapes_layer;
+$.getJSON("SEI.topojson", function (data) {
+    shapes_layer = L.topoJson(data, {
+
+        style: function (feature) {
+            sstyle = remove_highlight;
+            if (feature.properties.SE_ME_1 != "SE") {
+                sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+            } else {
+                sstyle.fillColor = typeSE[feature.properties.SECl_1];
+            }
+
+            return sstyle;
+        },
+        onEachFeature: function (feature, layer) {
+            var popUpInfo = feature.properties.Comp1Lgnd_;
+            if (feature.properties.Comp2Lgnd_) {
+                popUpInfo += "<br/>" + feature.properties.Comp2Lgnd_;
+                if (feature.properties.Comp3Lgnd_) {
+                    popUpInfo += "<br/>" + feature.properties.Comp3Lgnd_;
+                }
+            }
+            popUpInfo += "</br> Quality: " + feature.properties.QualityNo_ + "/5.0";
+            if (feature.properties.Location) {
+                popUpInfo += "</br> Location: " + feature.properties.Location;
+            }
+            // var popUp = layer.bindPopup(popUpInfo);
+            // popUp.on('popupclose', function() {
+            //     // layer.setStyle(remove_highlight);
+            // });
+            layer.on('click', function () {
+                layer.bringToFront();
+                if (layer.selected) {
+                    sstyle = remove_highlight;
+                    if (feature.properties.SE_ME_1 != "SE") {
+                        sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+                    } else {
+                        sstyle.fillColor = typeSE[feature.properties.SECl_1];
+                    }
+                    layer.setStyle(sstyle);
+                    layer.selected = 0;
+                    layer.unbindPopup();
+                } else {
+                    sstyle = highlight;
+                    // if (feature.properties.SE_ME_1 != "SE") {
+                    //     sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+                    // }
+                    // else {
+                    //     sstyle.fillColor = typeSE[feature.properties.SECl_1];
+                    // }
+                    layer.setStyle(sstyle);
+                    layer.selected = 1;
+                    layer.bindPopup(popUpInfo).openPopup();
+                }
+            })
+        }
+    })
+        .addTo(map);
+    overlays.SEI = shapes_layer;
+    if (countKeys(overlays) == num_overlays) {
+        L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
+    }
+});
+
+
+
+var points_layer_options = {
+    pointToLayer: function (feature, latlng) {
+        var marker = L.marker(latlng, {icon: dotIcon});
+        if (feature.properties) {
             var redList = feature.properties.redList;
             var markerPopUp = '<div class="popUpfeature">' + feature.properties.species + '<br/> <b>'
                 + feature.properties.common.split(',')[0] + '</b>';
@@ -118,7 +269,7 @@ $.getJSON("gbif_tot.geojson", function (data) {
 
             markerPopUp += '<br/>';
 
-            var id_species  = feature.properties.species;
+            var id_species = feature.properties.species;
             if (id_species) {
                 id_species = id_species.replace(/\s+/g, '_');
             }
@@ -135,107 +286,131 @@ $.getJSON("gbif_tot.geojson", function (data) {
 
             marker.bindPopup(markerPopUp);
 
-            marker.on('click', function() {
+            marker.on('click', function () {
                 loadImg(feature.properties.species);
             });
-
-            return marker;
         }
-    });
-    var clusters = L.markerClusterGroup(
-        {
-            iconCreateFunction: function (cluster) {
-                var i = 0;
-                var childrenMarkers = cluster.getAllChildMarkers();
-                var endangered = 0;
-                for (i = 0; i < childrenMarkers.length; i++) {
-                    if (childrenMarkers[i].feature.properties.redList) {
-                        endangered = 1;
-                        break;
-                    }
-                }
-                var num_children = cluster.getChildCount();
-                if (num_children == 1) {
-                    num_children = "";
-                }
-                if (endangered) {
-                    return L.divIcon({html: num_children, className: 'endangered', iconSize: 15});
-                } else {
-                    return L.divIcon({html: num_children, className: 'not_endangered', iconSize: 15});
-                }
-
-
-            }, singleMarkerMode: 1
-        }
-    );
-    clusters.addLayer(points_data);
-    map.addLayer(clusters);
-});
-
-// $.getJSON("red.geojson",function(data){
-//     var redStar = L.icon({
-//     iconUrl: 'red_star.png',
-//     iconSize: [20,20]
-//   });
-//   L.geoJson(data  ,{
-//     pointToLayer: function(feature,latlng){
-// 	  return L.marker(latlng,{icon: redStar, riseOnHover: 1});
-//     }
-//   }  ).addTo(map);
-// });
-
-var highlight = {
-    'color': '#333333',
-    'weight': 1,
-    'opacity': 1,
-    'fillColor': '#14bd00'
+        // var marker = L.marker(latlng, {title: ""});
+        return marker;
+    }
 };
 
-var remove_highlight = {
-    'color': "#515055",
-    'weight': 1,
-    'fillColor': "#8d8c91",
-    'fillOpacity': .5};
+// var points_layer = L.geoJson({ "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": { "Unnamed: 0": 70300, "datasetKey": "50c9509d-22c7-4a22-a47d-8c48425ef4a7", "kingdom": "Plantae", "phylum": "Tracheophyta", "class": "Magnoliopsida", "order": "Asterales", "family": "Asteraceae", "genus": "Tanacetum", "species": "Tanacetum vulgare", "decimalLatitude": 49.174106, "decimalLongitude": -123.120081, "eventDate": "2019-04-26T20:44:53Z", "day": 26.0, "month": 4.0, "year": 2019.0, "redList": null, "common": "common tansy, tansy, golden-buttons, garden tansy" }, "geometry": { "type": "Point", "coordinates": [ -123.120081, 49.174106 ] } },]},
+//                        points_layer_options );
+
+// var points_layer = L.geoJSON(null, points_layer_options);
+
+var clusters = L.markerClusterGroup(
+            {
+                iconCreateFunction: function (cluster) {
+                    var i = 0;
+                    var childrenMarkers = cluster.getAllChildMarkers();
+                    var endangered = 0;
+                    for (i = 0; i < childrenMarkers.length; i++) {
+                        if (childrenMarkers[i].feature.properties.redList) {
+                            endangered = 1;
+                            break;
+                        }
+                    }
+                    var num_children = cluster.getChildCount();
+                    if (num_children == 1) {
+                        num_children = "";
+                    }
+                    if (endangered) {
+                        return L.divIcon({html: num_children, className: 'endangered', iconSize: 15});
+                    } else {
+                        return L.divIcon({html: num_children, className: 'not_endangered', iconSize: 15});
+                    }
 
 
-// add shapes
-$.getJSON("SEI.geojson", function (data) {
-    L.geoJson(data, {
-        style: function (feature) {
-            return remove_highlight;
-        },
-        onEachFeature: function (feature, layer) {
-            var popUpInfo =  feature.properties.Comp1Lgnd_;
-            if (feature.properties.Comp2Lgnd_) {
-                popUpInfo += "<br/>" + feature.properties.Comp2Lgnd_;
-                if (feature.properties.Comp3Lgnd_) {
-                    popUpInfo += "<br/>" + feature.properties.Comp3Lgnd_;
-                }
+                }, singleMarkerMode: 1
             }
-            popUpInfo += "</br> Quality: " + feature.properties.QualityNo_ + "/5.0";
-            if (feature.properties.Location) {
-                popUpInfo += "</br> Location: " + feature.properties.Location;
-            }
-            // var popUp = layer.bindPopup(popUpInfo);
-            // popUp.on('popupclose', function() {
-            //     // layer.setStyle(remove_highlight);
-            // });
-            layer.on('click', function() {
-                layer.bringToFront();
-                if (layer.selected) {
-                    layer.setStyle(remove_highlight);
-                    layer.selected = 0;
-                    layer.unbindPopup();
-                }
-                else {
-                    layer.setStyle(highlight);
-                    layer.selected = 1;
-                    layer.bindPopup(popUpInfo).openPopup();
-                }
-            })
+        );
+clusters.addTo(map);
+        // var clusters_layer = map.addLayer(clusters);
+        // overlays.Gbif = clusters;
+        // if (countKeys(overlays) == num_overlays) {
+        //     L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
+        // }
+
+gbif_parts_filenames = ["UBC2010.geojson", "UBC2014.geojson", "UBC2018.geojson"];
+gbif_years = [];
+points_layers = [];
+var gbif_json_sent = [];
+
+function loadPointsJson(year) {
+    if (!gbif_json_sent[year]) {
+        $.getJSON("gbif/"+ year.toString() + ".geojson", function (data) {
+        // add GeoJSON layer to the map once the file is loaded
+
+        points_layers[year].addData(data);
+        clusters.addLayer(points_layers[year]);
+        // points_layer.addData(data);
+
+        // clusters.removeLayer(points_layer);
+            gbif_json_sent[year] = 1
+    });
+    }
+    else {
+        clusters.addLayer(points_layers[year]);
+    }
+
+}
+
+function deletePointsLayer(year) {
+    clusters.removeLayer(points_layers[year]);
+}
+
+var year_button_div = document.getElementById("year-buttons");
+
+var init_year = 1990;
+
+for (let i = init_year; i <= 2019; i++) {
+    gbif_json_sent[i] = 0;
+    gbif_years.push(i);
+    points_layers[i] = L.geoJSON(null, points_layer_options);
+    year_button = document.createElement("input");
+    year_button.setAttribute("type", "checkbox");
+    year_button.id = "year-" + i.toString();
+    year_button.onclick = function() {
+        if (this.checked) {
+             loadPointsJson(this.id.split('-')[1])
         }
-    })
-        .addTo(map);
-});
+        else {
+            deletePointsLayer(this.id.split('-')[1]);
+        }
+
+    };
+
+    year_label = document.createElement("label");
+    year_label.setAttribute("for", year_button.id);
+    year_label.innerText = i.toString();
+
+
+    year_button_div.appendChild(year_button);
+    year_button_div.appendChild(year_label);
+}
+//
+// map.addLayer(clusters);
+
+
+
+// gbif_years.forEach(function(year) {
+//     loadPointsJson("gbif/"+ year.toString() + ".geojson", year);
+// });
+
+
+
+
+
+
+
+
+
+
+
+// "#FF851B", "#01FF70","#FFDC00" ,"#F012BE",
+
+
 
 
